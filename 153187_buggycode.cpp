71e0b5700ed7c794f1e2a27b4b8e91d8f7ff93be
Sohail Asghar <yoyosohail121@gmail.com>
2017-10-01 00:14:38 +0500

    added the validation of input

--- 153187_buggycode.cpp
+++ 153187_buggycode.cpp
@@ -1,26 +1,35 @@
 // Read code below and try to identify any bug and then fix it. 
-#include <iostream>
+//added the header files
+#include<iostream>
 #include<conio.h>
 using namespace std;
 int main()
 {
 	cout << "This program will crash"
 		 << endl;
-
-	int nSum;
-	int nNums;
+// initialize the sum with the 0
+	float nSum=0.0;
+	float nNums;
 
 	// accumulate input numbers until the
 	// user enteres a negative number, then
 	// return the average
-	nNums = 0;
+	nNums = 0.0;
 	while(true)
 	{
 		// enter another number to add
+		label:
 		int nValue;
 		cout << "Enter another number:";
 		cin >> nValue;
 		cout << endl;
+		while(cin.fail()||cin.get() != '\n') {
+			cin.clear();
+		cin.clear();
+       cin.ignore(256,'\n');
+       cout<< "Please re-enter:  "<<endl;
+        goto label;
+    }
 
 		// if the input number is negative...
 		if(nValue < 0)
