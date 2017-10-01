39e3adc31d624defa8a312114778907f4a4b05cb
Talha Shabbir <talha2980@gmail.com>
2017-10-01 07:19:00 -0700

    Using float & finding average of point values

--- 153204buggycode.cpp
+++ 153204buggycode.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main()
 {
 
-	int nSum=0;
-	int nNums;
+	float nSum=0.0;
+	float nNums;
 
 	// accumulate input numbers until the
 	// user enteres a negative number, then
 	// return the average
-	nNums = 0;
+	nNums = 0.0;
 	while(true)
 	{
 		// enter another number to add
-		int nValue;
+		float nValue;
 		cout << "Enter another number:";
 		cin >> nValue;
 		cout << endl;
