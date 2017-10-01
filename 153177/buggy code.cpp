c9edca6448a8b584aa1decfc6fbb10a5c55a3f93
Fahad Ali Khan <aliapple1996@gmail.com>
2017-10-01 13:34:17 +0500

    Assingment.

---
+++ 153177/buggy code.cpp
@@ -0,0 +1,68 @@
+	#include<iostream>
+	#include<conio.h>
+	using namespace std;
+	int main()
+	{
+		
+		
+		cout << "Solution For Buggy Code"
+		
+		<< endl;	
+	
+			 
+			 
+		cout << "This program will crash"
+		
+			 << endl;
+			 
+	// initialize the sum with the 0
+		int nSum=0;
+		int nNums;
+	
+		// accumulate input numbers until the
+		// user enteres a negative number, then
+		// return the average
+		
+		nNums = 0;
+		
+		while(true)
+		
+		{
+			// enter another number to add
+
+			int nValue;
+			cout << "Enter another number:";
+			cin >> nValue;
+			cout << endl;
+	
+			// if the input number is negative...
+			
+		
+			if(nValue < 0)
+			{
+				// ... then output the average
+				cout << "Average is: "
+					 << nSum/nNums
+					 << endl;
+				break;
+			}
+			
+			
+	
+			// not negative, add the value to
+			// the accumulator
+			// incrementing for getting nNums to find average
+			
+			
+	                nNums++;
+			nSum += nValue;
+		}
+		
+		
+	
+		cin.ignore(10000, '\n');
+		return 0;
+	}
+	
+	//SOLVED THE MISTAKE.
+
