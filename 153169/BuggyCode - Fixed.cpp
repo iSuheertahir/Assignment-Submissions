d9e3b50fe00161c99995bb62516a9ccc2c3eb859
Ahmed Jamil <ahmed.jamil112233@gmail.com>
2017-10-01 23:51:20 +0500

    BuggyCode Fix 1 : Added Required Preprocessor Directories

---
+++ 153169/BuggyCode - Fixed.cpp
@@ -0,0 +1,42 @@
+//BuggyCode Fix 1 : Added Required Preprocessor Directories (153169 Ahmed Jamil)
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This program will crash"
+		 << endl;
+
+	int nSum;
+	int nNums;
+
+	// accumulate input numbers until the
+	// user enteres a negative number, then
+	// return the average
+	nNums = 0;
+	while(true)
+	{
+		// enter another number to add
+		int nValue;
+		cout << "Enter another number:";
+		cin >> nValue;
+		cout << endl;
+
+		// if the input number is negative...
+		if(nValue < 0)
+		{
+			// ... then output the average
+			cout << "Average is: "
+				 << nSum/nNums
+				 << endl;
+			break;
+		}
+
+		// not negative, add the value to
+		// the accumulator
+		nSum += nValue;
+	}
+
+	cin.ignore(10000, '\n');
+	return 0;
+}
