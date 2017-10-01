a7ec2b6cc01b58a5f650c48dd7079999fa0f45ab
Hamza fida <hamzafida37@gmail.com>
2017-10-01 12:46:31 -0700

    buggy code and error resolve

---
+++ 153181/BuggyCode/Buggy.cpp
@@ -0,0 +1,58 @@
+//Abdullah 
+//153181
+//Assignment # 03
+
+// Read code below and try to identify any bug and then fix it. 
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Now This program will not crash"
+		 << endl;
+
+	int nSum;
+	int nNums;
+
+	// accumulate input numbers until the
+	// user enteres a negative number, then
+	// return the average
+		nNums = 0;
+	nSum=0;
+
+	while(true)
+	{
+		int nValue;
+		
+		CheckInput:
+		
+		
+		cout << "Enter a number:";
+	
+		cin >> nValue;
+		cout << endl;
+		
+		//loop to check input
+		//while(!(cin.good())||cin.get() != '\n')
+
+		// if the input number is negative...
+		if(nValue < 0)
+		{
+			// ... then output the average
+			cout << "Average is: "
+				 << nSum/nNums
+				 << endl;
+			break;
+		}
+
+		// not negative, add the value to
+		// the accumulator
+		nSum += nValue;
+		++nNums;
+	}
+
+	cin.ignore(10000, '\n');
+	return 0;
+}
